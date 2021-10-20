f95cf84453bbecf92368307da7e95b1ca6f25675
giancarvalho <giandcarvalho@gmail.com>
2021-10-20 14:05:15 -0300

    created records display and componentized title

---
+++ src/components/RecordsDisplay.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+function Records() {
+    return (
+        <EntriesContainer>
+            <RecordList>
+                <Item>
+                    <Date>30/11</Date>
+                    <Details>Lunch Mother</Details> <Price>14.55</Price>
+                </Item>
+            </RecordList>
+            <BalanceContainer>
+                <h2>BALANCE</h2>
+                <Balance>2444.96</Balance>
+            </BalanceContainer>
+        </EntriesContainer>
+    );
+}
+
+export default function RecordsDisplay() {
+    const [records, setRecords] = useState([]);
+
+    function getRecords() {
+        //api records call
+    }
+
+    return (
+        <RecordsContainer>
+            {records.length > 0 ? (
+                <Records />
+            ) : (
+                <p>No income or expense records yet</p>
+            )}
+        </RecordsContainer>
+    );
+}
+
+const RecordsContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    flex-grow: 1;
+    background-color: #fff;
+    border-radius: 8px;
+    color: #868686;
+    padding: 12px 8px;
+`;
+
+const EntriesContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const BalanceContainer = styled.div`
+    width: 100%;
+    font-size: 16px;
+    display: flex;
+    justify-content: space-between;
+
+    h2 {
+        display: inline-block;
+        color: #000;
+        font-weight: 700;
+    }
+`;
+
+const Balance = styled.p`
+    color: #03ac00;
+`;
+
+const RecordList = styled.ul`
+    width: 100%;
+    flex-grow: 1;
+    overflow-y: scroll;
+    color: #000;
+    font-size: 15px;
+`;
+
+const Details = styled.div`
+    flex-grow: 1;
+`;
+
+const Date = styled.span`
+    color: #c6c6c6;
+    margin-right: 8px;
+`;
+
+const Price = styled.span`
+    color: #c70000;
+    align-self: flex-end;
+`;
+
+const Item = styled.li`
+    display: flex;
+    justify-content: space-between;
+`;
--- src/pages/MyWallet.js
+++ src/pages/MyWallet.js
@@ -3,6 +3,8 @@ import { PageContainer } from "../components/_shared/containers";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 import ActionButton from "../components/_shared/ActionButton";
+import RecordsDisplay from "../components/RecordsDisplay";
+import Title from "../components/_shared/Title";
 
 export default function MyWallet() {
     return (
@@ -14,9 +16,7 @@ export default function MyWallet() {
                         <RiLogoutBoxRLine />
                     </ActionButton>
                 </TitleContainer>
-                <RecordsContainer>
-                    {/* <p>No income or expense records yet</p> */}
-                </RecordsContainer>
+                <RecordsDisplay />
                 <EntriesButtonContainer>
                     <EntriesButton>
                         <AiOutlinePlusCircle />
@@ -41,26 +41,6 @@ const TitleContainer = styled.div`
     display: flex;
     width: 100%;
     justify-content: space-between;
-`;
-
-const Title = styled.h1`
-    display: inline-block;
-    font-size: 24px;
-    font-weight: 700;
-    color: #fff;
-    margin-bottom: 20px;
-`;
-
-const RecordsContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    flex-grow: 1;
-    background-color: #fff;
-    border-radius: 8px;
-    color: #868686;
-    padding: 8px;
 `;
 
 const EntriesButtonContainer = styled.div`
@@ -90,8 +70,4 @@ const EntriesButton = styled.div`
     svg {
         font-size: 23px;
     }
-`;
-
-const EntriesContainer = styled.div`
-    width: 100%;
 `;
